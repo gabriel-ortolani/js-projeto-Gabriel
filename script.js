0dd16ceaf9021a31a5463b88814e0ed9cda00d82
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2025-03-25 10:29:34 -0300

    código atualizado

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Digitação (JS Completo)
+// controle do menu mobile
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+menuIcon.addEventListener('click',() => {
+    menuIcon.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+    
+    // Bloquear o scroll quando o menu estiver aberto
+    document.body.style.overflow = navList.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+// Fechar menu ao clicar nos links
+document.querySelector('.navlist a').forEach(link => { link.addEventListener('click', () => {
+    menuIcon.classList.remove('bx-x');
+    navList.classList.remove('open');
+    document.body.style.overflow = 'auto';
+
+    });
+});
+
+// Fechar ao rolar a página
+window.addEventListener('scroll', () =>{
+    if(navList.classList.contains('open')) {
+        menuIcon.classList.remove('bx-x');
+        navList.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    }
+});
