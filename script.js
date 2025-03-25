63888459aa7b0284d8085bd9457186c5a9bcfba7
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2025-03-25 11:03:49 -0300

    .

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ menuIcon.addEventListener('click',() => {
 });
 
 // Fechar menu ao clicar nos links
-document.querySelector('.navlist a').forEach(link => { link.addEventListener('click', () => {
+document.querySelectorAll('.navlist a').forEach(link => { link.addEventListener('click', () => {
     menuIcon.classList.remove('bx-x');
     navList.classList.remove('open');
     document.body.style.overflow = 'auto';
@@ -28,3 +28,36 @@ window.addEventListener('scroll', () =>{
         document.body.style.overflow = 'auto';
     }
 });
+
+//  ===== Navegação ativa =====
+// Seleciona todos os links de navegação
+const navLinks = document.querySelectorAll('.navlist a');
+
+// Função para adicionar a classe 'active' no link clicado
+function activeLink(){
+    navLinks.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+// adiciona o evento de click no link de navegação
+navLinks.forEach(item => item.addEventListener('click', activeLink));
+
+// ===== Alterar Modo claro / modo escuro =====
+// Função para alterar entre os temas
+function toggleMode(){
+    const html = document.documentElement;
+    html.classList.toggle('light');
+
+    // salva o tema escolhido no localStorege
+    const mode = html.classList.contains('light') ? 'light' : 'dark';
+    localStorage.setItem('theme', mode);
+
+    // Alterar apaência do título
+    updateTextColor();
+}
+
+// Função que altera a cor do texto de acordo com o tema
+function updateTextColor(){
+    currentColor = document.document.classList.contains('light') ? 'black' : '#fff';
+    titleElement.style.color = currentColor;
+}
