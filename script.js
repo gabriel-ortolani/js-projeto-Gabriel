17c34aa5c3ba6bbac4ff1311a73e8b444202caef
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2025-03-25 12:15:29 -0300

    .

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ if(savedTheme){
 // ===== Animação do titulo principal =====
 // Seleciona o elemento 'titulo' e define as variaveis para animar
 const titleElement = document.querySelector('#name');
-const text = "Gabriel Ortolani";
+const text = "Gabriel";
 let index = 0;
 let isTyping = true;
 let currentColor = document.documentElement.classList.contains('light') ? 'black' : '#fff'
@@ -104,4 +104,50 @@ function animateText(){
 document.addEventListener('DOMContentLoaded', animateText);
 updateTextColor();
 
+// ===== animação da sessão home =====
+// Seleciona a seção home e aplica uma animação de fade-in
+const homeSection = document.querySelector('#home');
+homeSection.style.opacity = '0';
+homeSection.style.transform= 'translateY(20px)';
+homeSection.style.transition = 'opacity 1s ease, transform 1s ease';
 
+setTimeout(() => {
+    homeSection.style.opacity = '1';
+    homeSection.style.transform = 'translate(0)';
+}, 100);
+
+// ===== ANIMAÇÃO DA SEÇÕES =====
+// Seleciona todas as seções e aplica animações de entrada
+const sections = document.querySelectorAll('section');
+
+sections.forEach((section, index) => {
+    section.style.opacity = '0';
+    section.style.transition = 'opacity 1s, transform 1s';
+
+    // aplica diferentes transformações com base mo indice da seção
+    if(index !== 0){
+        if(index === 1) section.style.transform = 'transleteY(100px)';
+        else if(index === 2) section.style.transform = 'scale(0.8)';
+        else if(index === 3) section.style.transform = 'rotateY(90deg)';
+    }
+});
+
+// observer para animar a seções ao rolar a página
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'nome';
+        }
+    });
+});
+
+// observa cada seção para aplicar  animação
+sections.forEach((section) => observer.observe(section));
+
+// ===== BOTÃO DE VOLTAR AO TOPO =====
+// Adiciona um evento de clique ao botão de voltar ao topo
+document.querySelector('.top a').addEventListener('click', (e) =>{
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth'}); // Rola suavemente para o topo da página 
+});
